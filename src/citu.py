c6d1596a4a9319565a05d711d63d471ecc178b93
Michel Betancourt <MichelBetancourt23@gmail.com>
2017-10-13 21:09:06 -0400

    first commit

---
+++ src/citu.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2017 akhail
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
